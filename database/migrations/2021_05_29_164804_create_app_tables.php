3f45fedef5a600cd2f4500759d7a834b07f03963
tsunga59 <tatsumochi15@gmail.com>
2021-05-31 10:33:50 +0900

    商品出品機能を作成

--- database/migrations/2021_05_29_164804_create_app_tables.php
+++ database/migrations/2021_05_29_164804_create_app_tables.php
@@ -15,12 +15,16 @@ class CreateAppTables extends Migration
     {
         Schema::create('primary_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('sort_no');
             $table->timestamps();
         });
 
         Schema::create('secondary_categories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('primary_category_id');
+            $table->string('name');
+            $table->integer('sort_no');
             $table->timestamps();
 
             $table->foreign('primary_category_id')->references('id')->on('primary_categories');
@@ -28,17 +32,26 @@ class CreateAppTables extends Migration
 
         Schema::create('item_conditions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('sort_no');
             $table->timestamps();
         });
 
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('seller_id');
-            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('buyer_id')->nullable();
             $table->unsignedBigInteger('secondary_category_id');
             $table->unsignedBigInteger('item_condition_id');
+            
+            $table->string('name');
+            $table->string('image_file_name');
+            $table->text('description');
+            $table->unsignedBigInteger('price');
+            $table->string('state');
+            
             $table->timestamps();
-
+            
             $table->foreign('seller_id')->references('id')->on('users');
             $table->foreign('buyer_id')->references('id')->on('users');
             $table->foreign('secondary_category_id')->references('id')->on('secondary_categories');
